f7bdd5d0ed73681050473a0e9e259cabe1c3af8d
João Pedro Campos <jpgoncalves1406@gmail.com>
2026-08-21 11:10:45 -0300

    Aula 02

--- aulas/aula02/main.c
+++ aulas/aula02/main.c
@@ -2,6 +2,7 @@
 #include "grafo_matriz.h"
 
 int main() {
+    // matriz de adjacencia usa vertices com verticer (posição com posição) 
     GrafoMatriz grafo;
 
     inicializar(&grafo, 8);
@@ -15,18 +16,30 @@ int main() {
     inserir_aresta(&grafo, 2, 6);
     inserir_aresta(&grafo, 3, 6);
     inserir_aresta(&grafo, 7, 4);
-    inserir_aresta(&grafo, 7, 5);
     inserir_aresta(&grafo, 7, 6);
 
-    printf("Matriz de Adjacencia - grafo não orientado\n");
+    printf("Matriz de Adjacencia\n"); 
     exibir_matriz(&grafo);
 
+    // Exemplo de grafo com arcos (direcionado)
     inicializar(&grafo, 3);
-    inserir_arco(&grafo, 0, 1);
+    /* as lihas e colunas começam em zero. ent o tamanho 3x3 serão:
+
+            coluna coluna coluna
+               0     1     1
+    linha 0 | 0,0 | 0,1 | 0,2
+    linha 1 | 1,0 | 1,1 | 1,2
+    linha 2 | 2,0 | 2,1 | 2,2
+
+    */
+
+    inserir_arco(&grafo, 0, 1); 
     inserir_arco(&grafo, 1, 2);
     inserir_arco(&grafo, 2, 0);
 
-    printf("Matriz de Adjacencia - grafo orientado\n");
+
+    printf("\nMatriz de Adjacencia (grafo direcionado)\n");
+
     exibir_matriz(&grafo);
 
     return 0;
